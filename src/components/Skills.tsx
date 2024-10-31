40de57e9530d06b92c533e44e2e1bb18965eab3f
AmjadRasheed7 <1211307882@student.mmu.edu.my>
2024-10-31 20:34:24 +0800

    fixed skill container

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,7 +4,7 @@ function Skills() {
   return (
     <div className="md:p-5 md:px-16 px-6 skills bg-[#121212] h-[100vh]">
       <h1 className="md:text-6xl py-4 text-4xl text-white font-poppins font-bold md:my-6">Skills</h1>
-      <div className="container md:px-64 md:p-2 flex flex-wrap justify-center items-center gap-4">
+      <div className="container mx-auto md:px-64 md:p-2 flex flex-wrap justify-center items-center gap-4">
       <SkillBall icon="test" ></SkillBall>
       <SkillBall icon="test" ></SkillBall>
       <SkillBall icon="test" ></SkillBall>
